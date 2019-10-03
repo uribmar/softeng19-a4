1d4f098f0d12b72df7a03b0d05423b3fb0e5a92a
Jason Shepherd <jason_b_shepherd@acm.org>
2019-10-03 14:27:05 -0500

    A4 starting point

--- pmgr/project.py
+++ pmgr/project.py
@@ -15,14 +15,31 @@ class Project(object):
         self.filepath =  projdir / (self.name + '.txt')
         if not self.filepath.exists():
             self.filepath.touch()
-        
 
     def add_task(self, task_name):
-        pass
+        tasks = self.get_tasks()
+        if task_name in tasks:
+            raise TaskException('Task "{}" already exists.'.format(task_name))
+
+        with open(str(self.filepath), 'a') as wf:
+            wf.write(task_name + '\n')
 
     def remove_task(self, task_name):
-        pass
+        tasks = self.get_tasks()
+        if task_name not in tasks:
+            raise TaskException('Task "{}" does not exist.'.format(task_name))
+        else:
+            tasks.remove(task_name)
+
+        with open(str(self.filepath), 'w') as wf:
+            for task in tasks:
+                wf.write(task + '\n')
 
     def get_tasks(self):
-        pass
+        tasks = []
+        with open(str(self.filepath), 'r') as rf:
+            for line in rf:
+                tasks.append(line.rstrip())
+        return tasks
+
 
