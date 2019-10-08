c26cf88882d55ace1c8c816c3f19741619960437
Marco Uribe <mxloco27@gmail.com>
2019-10-07 19:23:54 -0500

    added empty test functions to implement

---
+++ tests/tests.py
@@ -0,0 +1,28 @@
+from pmgr.project import Project, TaskException
+
+#FIXME don't know if we need to test the functions in main or not
+#from pmgr.__main__ import show, add, remove
+
+#TODO
+def test_project_init():
+    pass
+
+#TODO
+def test_project_add():
+    pass
+
+#TODO
+def test_project_add_fail():
+    pass
+
+#TODO
+def test_project_remove():
+    pass
+
+#TODO
+def test_project_remove_fail():
+    pass
+
+#TODO
+def test_project_get():
+    pass
